74c89902e68bd8cc61fa2c55321cc0b1e7b8a932
AMIT PATTANAYAK <122168162+AMITPATTANAYAK@users.noreply.github.com>
2023-05-05 14:46:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ let songs = [
     {songName: "Ek Din Teri Raahon", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "I Love You", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Hasi-Female Version", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Mitti Di Khushboo", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Pyaar Ki Ek Kahani", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Phir Mohabbat", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Dil Ibaadat", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Saans", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Naam Na Jana Pakhi-Female Version", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
