525a6dfd0c6121092b3ed3744587d2a257338131
AMIT PATTANAYAK <122168162+AMITPATTANAYAK@users.noreply.github.com>
2023-05-05 13:37:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
     {songName: "Uska Hi Banana", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Ek Din Teri Raahon", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "I Love You", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
